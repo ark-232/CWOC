83ebdef21345a0160b0bd00756372cdcc429bd6e
David <petzoldavid02@gmail.com>
2022-07-12 19:45:02 -0600

    I figured it out

--- script.py
+++ script.py
@@ -1,18 +1,29 @@
 import functions
-import datetime
-import wave
+# import datetime
+# import wave
 from playsound import playsound
+import pyttsx3
+engine = pyttsx3.init() # object creation
 
 
-user_input = input()
-if user_input == "exit":
-    print("Goodbye!")
-    exit()
-elif user_input == "play":
-    print("Playing...")
-    playsound("Reville.mp3")
-    exit()
-elif user_input == "warn":
-    print("Warning!")
-    playsound('warning.wav')
-    exit()
+#make a loop that keeps the program running until the user wants to quit
+while True:
+    user_input = input()
+    if user_input == "exit":
+        print("Goodbye!")
+    elif user_input == "play":
+        print("Playing...")
+        playsound("Reville.wav")
+    elif user_input == "warn":
+        print("Warning!")
+        playsound('warning.wav')
+    #if the user inputs stop or exit, the program will stop
+    elif user_input == "stop":
+        print("Stopping...")
+        exit()
+    #make a text to speach function that will take the user input and speak it
+    elif user_input == "speak":
+        print("What would you like to speak?")
+        user_input = input()
+        engine.say(user_input)
+        engine.runAndWait()
