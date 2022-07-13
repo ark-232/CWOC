d789066faf5ab84f141f2bf405e09dfbc562b05b
ark-232 <arkady232@gmail.com>
2022-07-12 19:31:31 -0600

    more ifelse

--- script.py
+++ script.py
@@ -8,3 +8,11 @@ user_input = input()
 if user_input == "exit":
     print("Goodbye!")
     exit()
+elif user_input == "play":
+    print("Playing...")
+    playsound("Reville.mp3")
+    exit()
+elif user_input == "warn":
+    print("Warning!")
+    playsound('warning.wav')
+    exit()
