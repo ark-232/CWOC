b7b8a216a109cbca5c8b057dce2e624b4d601338
David <petzoldavid02@gmail.com>
2022-07-12 19:51:07 -0600

    Clean

--- script.py
+++ script.py
@@ -5,22 +5,25 @@ from playsound import playsound
 import pyttsx3
 engine = pyttsx3.init() # object creation
 
-
 #make a loop that keeps the program running until the user wants to quit
 while True:
     user_input = input()
     if user_input == "exit":
         print("Goodbye!")
+
     elif user_input == "play":
         print("Playing...")
         playsound("Reville.wav")
-    elif user_input == "warn":
-        print("Warning!")
-        playsound('warning.wav')
+
+    elif user_input == "lightning":
+        engine.say("Lightning within 5 nautical miles!")
+        engine.runAndWait()
+
     #if the user inputs stop or exit, the program will stop
     elif user_input == "stop":
         print("Stopping...")
         exit()
+
     #make a text to speach function that will take the user input and speak it
     elif user_input == "speak":
         print("What would you like to speak?")
